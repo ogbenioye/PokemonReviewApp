4955f532e4d22cc5021193fb21131934e757df73
Oluwapelumi Oyenuga <oluwapelumioyenuga@gmail.com>
2023-12-12 18:48:08 +0100

    add database

--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Data;
+
 namespace PokemonReviewApp
 {
     public class Program
@@ -12,6 +15,10 @@ namespace PokemonReviewApp
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
